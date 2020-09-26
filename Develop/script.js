37d2bb24a28b82d686bfbfa5c65476dbcf07fae2
jcuush <jrcushing@sbcglobal.net>
2020-09-25 19:51:46 -0500

    added for loop to prompt asking for criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
+var passCriteria = ["Lowercase", "Uppercase", "Special Characters", "Numbers"]
+
+for (var i = 0; i < passCriteria.length; i++) {
+  var userInput = prompt("Do you want to use " +passCriteria[i] +"?" +" Type Yes or No");
+  userInput = userInput.toLowerCase();
+
+  if (passCriteria[i]==="yes") {
+    
+  }
+
+
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +21,11 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// function writePassword(){
+//   document.getElementById("password").innerHTML = "NICE";
+// }
